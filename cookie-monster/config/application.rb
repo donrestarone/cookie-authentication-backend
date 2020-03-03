623e7a3c47a4cacf5d10946454278f02a3f877a5
donrestarone <shashikejayatunge@gmail.com>
2020-03-02 20:10:41 -0500

    add it to the api stack and include it in application controller

--- cookie-monster/config/application.rb
+++ cookie-monster/config/application.rb
@@ -33,5 +33,6 @@ module CookieMonster
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
   end
 end
